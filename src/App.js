e13c7213b46c831bf4edfcddaa912bd584c9a197
AlbertoPlascencia <144484832+AlbertoPlascencia@users.noreply.github.com>
2023-09-08 12:48:08 -0600

    1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Citas de uñas</h1>
+      <p>Bienvenidos</p>
+    </div>
+  );
+}
+
+export default App;
